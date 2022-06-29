778274678ab20a342a4a0096047b6873ae0398fa
Xie Yuheng <xyheme@gmail.com>
2022-06-29 21:25:40 +0800

    up

--- components/pomodoro/PomodoroHeader.tsx
+++ components/pomodoro/PomodoroHeader.tsx
@@ -3,7 +3,7 @@ import { observer } from "mobx-react-lite"
 import { PomodoroState as State } from "./PomodoroState"
 
 export const PomodoroHeader: FC<{ state: State }> = observer(({ state }) => (
-  <div className="flex w-full items-center justify-between border-b border-red-300 px-4 py-4 font-sans text-3xl font-semibold">
+  <div className="flex w-full items-center border-b border-red-300 px-4 py-4 font-sans text-3xl font-semibold">
     <div>Pomodoro</div>
   </div>
 ))
