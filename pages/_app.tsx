60bfcc10b7cdfe0b688ebdde8a4c3da62247d5c3
Xie Yuheng <xyheme@gmail.com>
2022-06-29 18:09:51 +0800

    up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,24 @@
 import type { AppProps } from "next/app"
 import "../styles/index.css"
 
+import { configure } from 'mobx';
+
+configure({
+  enforceActions: 'never',
+});
+
+// Use spec compliant transpilation for class properties
+// - https://mobx.js.org/installation.html
+
+if (
+  !new (class {
+    constructor(public x: string = 'x') {}
+  })().hasOwnProperty('x')
+) {
+  throw new Error('Transpiler is not configured correctly');
+}
+
+
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
