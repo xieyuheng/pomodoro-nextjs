7aa72cc053b54417377041d41b31197c2f1f38cf
Xie Yuheng <xyheme@gmail.com>
2022-06-29 20:46:09 +0800

    up

--- components/pomodoro/Pomodoro.tsx
+++ components/pomodoro/Pomodoro.tsx
@@ -4,11 +4,9 @@ import { PomodoroState as State } from "./PomodoroState"
 import { PomodoroTimer } from "./PomodoroTimer"
 
 export const Pomodoro: FC<{ state: State }> = observer(({ state }) => (
-  <div>
-    <div className="h-screen w-screen bg-red-400 text-white">
-      <div className="flex h-full flex-col items-center justify-center">
-        <PomodoroTimer state={state} />
-      </div>
+  <div className="h-screen w-screen bg-red-400 text-white">
+    <div className="flex h-full flex-col items-center py-10">
+      <PomodoroTimer state={state} />
     </div>
   </div>
 ))
