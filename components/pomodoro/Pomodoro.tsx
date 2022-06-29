5c58798e7dc6bb283aea16ddc1796b78fb74bd87
Xie Yuheng <xyheme@gmail.com>
2022-06-29 18:08:04 +0800

    up

--- components/pomodoro/Pomodoro.tsx
+++ components/pomodoro/Pomodoro.tsx
@@ -1,8 +1,8 @@
 export default function Pomodoro() {
   return (
     <div>
-      <div class="h-screen w-screen bg-red-400 text-white">
-        <div class="h-full flex flex-col justify-center items-center">
+      <div className="h-screen w-screen bg-red-400 text-white">
+        <div className="h-full flex flex-col justify-center items-center">
           <div className="text-red-200 text-2xl">Pomodoro</div>
         </div>
       </div>
