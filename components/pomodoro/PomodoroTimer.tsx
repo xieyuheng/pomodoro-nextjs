5958a8d26d2f766ae4e1dff6e25838d3694fb279
Xie Yuheng <xyheme@gmail.com>
2022-06-29 18:38:36 +0800

    up

--- components/pomodoro/PomodoroTimer.tsx
+++ components/pomodoro/PomodoroTimer.tsx
@@ -1,10 +1,11 @@
+import * as React from "react"
 import { PomodoroState as State } from "./PomodoroState"
 import { observer } from "mobx-react-lite"
 
-export const PomodoroTimer = observer(({ state }) => {
-  return (
+export const PomodoroTimer: React.FC<{ state: State }> = observer(
+  ({ state }) => (
     <div className="text-9xl">
       {state.formatMinutes()}:{state.formatSeconds()}
     </div>
   )
-})
+)
