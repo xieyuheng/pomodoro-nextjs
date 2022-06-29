8089cdd14f85d3fc0f0330d41b917b92cc1a5471
Xie Yuheng <xyheme@gmail.com>
2022-06-29 20:46:26 +0800

    up

--- components/pomodoro/PomodoroTimer.tsx
+++ components/pomodoro/PomodoroTimer.tsx
@@ -4,7 +4,7 @@ import { PomodoroState as State } from "./PomodoroState"
 import { PomodoroTimerControl } from "./PomodoroTimerControl"
 
 export const PomodoroTimer: FC<{ state: State }> = observer(({ state }) => (
-  <div className="flex flex-col items-center border-2 border-red-600 bg-red-500 rounded bg-red-400 py-6 px-2 md:py-10 md:px-6">
+  <div className="flex flex-col items-center rounded border-2 border-red-600 bg-red-500 bg-red-400 py-6 px-2 md:py-10 md:px-6">
     <div className="font-mono text-8xl font-semibold md:text-9xl">
       {state.formatMinutes()}:{state.formatSeconds()}
     </div>
