41f789e6d9a6fe5114aa72065bd328ab0c9cb699
Xie Yuheng <xyheme@gmail.com>
2022-06-30 17:26:08 +0800

    up

--- src/components/pomodoro/Settings.ts
+++ src/components/pomodoro/Settings.ts
@@ -14,10 +14,18 @@ export class Settings {
   }
 
   static defaultSettings(): Settings {
+
+    return new Settings({
+      modes: {
+        Focus: new Focus(5),
+        Break: new Break(5),
+        Recess: new Recess(5),
+      },
+    })
+
     return new Settings({
       modes: {
         Focus: new Focus(25 * 60),
-
         Break: new Break(10 * 60),
         Recess: new Recess(20 * 60),
       },
