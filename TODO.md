43f4a3fa8df197a4760f6bb9ad8f93df79ffbc00
Xie Yuheng <xyheme@gmail.com>
2022-07-06 18:59:39 +0800

    - `Task` -- `id`

--- TODO.md
+++ TODO.md
@@ -1,6 +1,12 @@
-- `state.tasks`
+- `PomodoroState` -- `selectTask`
+- `PomodoroTaskItem` -- click to switch current task
+
+- `PomodoroTaskForm`
+
 - `PomodoroState` handle current task
+
 - `PomodoroTasks` -- show current task name in page title
+
 - be able to config `settings.modes`
 - use `localStorage` to store `settings`
 - `useLang` -- with `lang` -- to suppurt both English and Chinese
--- src/components/pomodoro/PomodoroState.ts
+++ src/components/pomodoro/PomodoroState.ts
@@ -9,24 +9,28 @@ import { Settings } from "./Settings"
 type Timer = ReturnType<typeof setInterval>
 
 interface Task {
+  id: number
   title: string
 }
 
 const testingTasks = [
-  { title: "Lorem ipsum dolor sit amet" },
-  { title: "consectetur adipiscing elit" },
-  { title: "Ut enim ad minim veniam" },
+  { id: 1, title: "Lorem ipsum dolor sit amet" },
+  { id: 2, title: "consectetur adipiscing elit" },
+  { id: 3, title: "Ut enim ad minim veniam" },
   {
+    id: 4,
     title:
       "quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
   },
   {
+    id: 5,
     title:
       "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur",
   },
 ]
 
 const testingCurrentTask = {
+  id: 6,
   title: "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
 }
 
