e4f031d756d5f5fecaf97496b6e85a2aceae3420
Xie Yuheng <xyheme@gmail.com>
2022-07-06 18:54:31 +0800

    up

--- src/components/pomodoro/PomodoroTaskCurrent.tsx
+++ src/components/pomodoro/PomodoroTaskCurrent.tsx
@@ -16,11 +16,11 @@ export default observer(function PomodoroTaskCurrent({
         "flex flex-col border-8 p-3 text-2xl font-semibold md:py-4",
         state.classes.transition,
         {
-          "border-focus-300  bg-focus-200  text-focus-900":
+          "border-focus-300  bg-focus-100  text-focus-900":
             state.kind === "Focus",
-          "border-break-300  bg-break-200  text-break-900":
+          "border-break-300  bg-break-100  text-break-900":
             state.kind === "Break",
-          "border-recess-300 bg-recess-200 text-recess-900":
+          "border-recess-300 bg-recess-100 text-recess-900":
             state.kind === "Recess",
         }
       )}
