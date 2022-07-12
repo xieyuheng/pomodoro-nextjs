81ad840fd74484a318d4a6845be0a3230d6d6a13
Xie Yuheng <xyheme@gmail.com>
2022-07-12 10:32:09 +0800

    up

--- src/views/pomodoro/usePomodoroState.ts
+++ src/views/pomodoro/usePomodoroState.ts
@@ -4,20 +4,14 @@ export function usePomodoroState(): [
   PomodoroState,
   (state: PomodoroState) => void
 ] {
-  let state = new PomodoroState()
-  let saveState: (state: PomodoroState) => void = () => {}
-
-  if (typeof window === "undefined") {
-    return [state, saveState]
-  }
-
   const found = window.localStorage.getItem(PomodoroState.name)
-  if (found) {
-    state = PomodoroState.create(JSON.parse(found))
-    saveState = (state) => {
-      localStorage.setItem(PomodoroState.name, JSON.stringify(state.json()))
-      console.log(state.json())
-    }
+
+  const state = found
+    ? PomodoroState.create(JSON.parse(found))
+    : new PomodoroState()
+
+  const saveState = (state: PomodoroState) => {
+    localStorage.setItem(PomodoroState.name, JSON.stringify(state.json()))
   }
 
   return [state, saveState]
