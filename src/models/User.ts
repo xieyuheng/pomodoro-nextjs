9d1d690d7a1e5223f27b574638bb9daee5fd07e9
Xie Yuheng <xyheme@gmail.com>
2022-07-17 10:10:23 +0800

    up

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,6 +2,7 @@ import { Entity, Schema } from "redis-om"
 import { client, connect } from "../lib/redis"
 
 export type UserJson = {
+  username: string
   name: string
   email: string
 }
@@ -10,6 +11,7 @@ export interface User extends UserJson {}
 
 export class User extends Entity {
   static schema = new Schema(User, {
+    username: { type: "string" },    
     name: { type: "string" },
     email: { type: "string" },
   })
--- src/pages/api/users.ts
+++ src/pages/api/users.ts
@@ -3,6 +3,7 @@ import { User } from "../../models/User"
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
   const user = await User.create({
+    username: "xieyuheng",
     name: "Xie Yuheng",
     email: "xyheme@gmail.com",
   })
