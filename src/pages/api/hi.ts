798c6e71b860d42759ecca9cff3304d2cc03071c
Xie Yuheng <xyheme@gmail.com>
2022-07-17 09:33:45 +0800

    up

--- src/pages/api/hi.ts
+++
@@ -1,5 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next"
-
-export default async function (req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).send("hello there ~")
-}
--- src/pages/api/users.ts
+++ src/pages/api/users.ts
@@ -3,5 +3,5 @@ import { createUser } from "../../models/User"
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
   const id = await createUser()
-  res.status(200).send(id)
+  res.status(200).json({ id })
 }
