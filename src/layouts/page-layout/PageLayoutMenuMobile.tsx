32c46838779f36d278cc105d35dbf1a258476ce7
Xie Yuheng <xyheme@gmail.com>
2022-07-20 06:56:36 +0800

    up

--- src/layouts/page-layout/PageLayoutMenuMobile.tsx
+++ src/layouts/page-layout/PageLayoutMenuMobile.tsx
@@ -19,10 +19,10 @@ export default observer(function PageLayoutMenuMobile({
 
       <Transition
         as={Fragment}
-        enter="transition duration-450"
+        enter="transition duration-300"
         enterFrom="transform opacity-0 translate-x-6 rotate-45"
         enterTo="transform opacity-100 rotate-0"
-        leave="transition duration-350"
+        leave="transition duration-300"
         leaveFrom="transform opacity-100"
         leaveTo="transform opacity-0 translate-x-6"
       >
