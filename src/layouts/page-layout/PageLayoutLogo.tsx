4ee441ac62d3e89d8c35cfee96d5ab50dd332b49
Xie Yuheng <xyheme@gmail.com>
2022-07-20 06:20:40 +0800

    up

--- src/layouts/page-layout/PageLayoutLogo.tsx
+++ src/layouts/page-layout/PageLayoutLogo.tsx
@@ -1,13 +1,11 @@
 import { observer } from "mobx-react-lite"
 import { PageLayoutState as State } from "./PageLayoutState"
-import Link from "next/link"
+import Link from "../../components/Link"
 
 export default observer(function PageLayoutLogo({ state }: { state: State }) {
   return (
-    <Link href="/">
-      <a className="text-2xl font-bold hover:underline md:text-3xl">
-        {state.appName}
-      </a>
+    <Link href="/" className="text-2xl font-bold hover:underline md:text-3xl">
+      {state.appName}
     </Link>
   )
 })
