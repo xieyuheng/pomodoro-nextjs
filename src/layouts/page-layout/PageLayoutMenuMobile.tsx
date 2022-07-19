df51ce8bbeb85d168cd85bf60f06427b12b12ed0
Xie Yuheng <xyheme@gmail.com>
2022-07-20 06:48:06 +0800

    up

--- src/layouts/page-layout/PageLayoutMenuMobile.tsx
+++ src/layouts/page-layout/PageLayoutMenuMobile.tsx
@@ -1,3 +1,4 @@
+import { Fragment } from "react"
 import classes from "classnames"
 import { observer } from "mobx-react-lite"
 import { PageLayoutState as State } from "./PageLayoutState"
@@ -16,60 +17,70 @@ export default observer(function PageLayoutMenuMobile({
         <MenuIcon className="h-5 w-5" />
       </Menu.Button>
 
-      <Menu.Items
-        className={classes(
-          "fixed top-0 right-0 h-screen w-screen border-8 p-4",
-          `bg-${state.theme.name}-400 border-${state.theme.name}-300`
-        )}
+      <Transition
+        as={Fragment}
+        enter="transition duration-350"
+        enterFrom="transform opacity-0 translate-x-6"
+        enterTo="transform opacity-100"
+        leave="transition duration-200"
+        leaveFrom="transform opacity-100"
+        leaveTo="transform opacity-0 translate-x-6"
       >
-        <div className={classes("flex justify-end")}>
-          <Menu.Item>
-            {({ active }) => (
-              <XIcon
-                className={classes(
-                  "h-10 w-10 text-right",
-                  active && `border-4 border-${state.theme.name}-200`,
-                  active && `text-${state.theme.name}-200`
-                )}
-              />
-            )}
-          </Menu.Item>
-        </div>
+        <Menu.Items
+          className={classes(
+            "fixed top-0 right-0 h-screen w-screen border-8 p-4",
+            `bg-${state.theme.name}-400 border-${state.theme.name}-300`
+          )}
+        >
+          <div className={classes("flex justify-end")}>
+            <Menu.Item>
+              {({ active }) => (
+                <XIcon
+                  className={classes(
+                    "h-10 w-10 text-right",
+                    active && `border-4 border-${state.theme.name}-200`,
+                    active && `text-${state.theme.name}-200`
+                  )}
+                />
+              )}
+            </Menu.Item>
+          </div>
 
-        <div className="py-10">
-          <Menu.Item>
-            {({ active }) => (
-              <Link
-                href="/register"
-                className={classes(
-                  "flex justify-center p-2 font-semibold",
-                  active && "underline decoration-6",
-                  active && `text-${state.theme.name}-200`
-                )}
-              >
-                {state.lang.zh && "注册"}
-                {state.lang.en && "REGISTER"}
-              </Link>
-            )}
-          </Menu.Item>
+          <div className="py-10">
+            <Menu.Item>
+              {({ active }) => (
+                <Link
+                  href="/register"
+                  className={classes(
+                    "flex justify-center p-2 font-semibold",
+                    active && "underline decoration-6",
+                    active && `text-${state.theme.name}-200`
+                  )}
+                >
+                  {state.lang.zh && "注册"}
+                  {state.lang.en && "REGISTER"}
+                </Link>
+              )}
+            </Menu.Item>
 
-          <Menu.Item>
-            {({ active }) => (
-              <Link
-                href="/login"
-                className={classes(
-                  "flex justify-center p-2 font-semibold",
-                  active && "underline decoration-6",
-                  active && `text-${state.theme.name}-200`
-                )}
-              >
-                {state.lang.zh && "登录"}
-                {state.lang.en && "LOGIN"}
-              </Link>
-            )}
-          </Menu.Item>
-        </div>
-      </Menu.Items>
+            <Menu.Item>
+              {({ active }) => (
+                <Link
+                  href="/login"
+                  className={classes(
+                    "flex justify-center p-2 font-semibold",
+                    active && "underline decoration-6",
+                    active && `text-${state.theme.name}-200`
+                  )}
+                >
+                  {state.lang.zh && "登录"}
+                  {state.lang.en && "LOGIN"}
+                </Link>
+              )}
+            </Menu.Item>
+          </div>
+        </Menu.Items>
+      </Transition>
     </Menu>
   )
 })
